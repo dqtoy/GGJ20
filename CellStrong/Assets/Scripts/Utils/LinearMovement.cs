e8be16daa73db2d7ebb6a5237a790d1a8af76167
Siyuan <finalspace.1031@gmail.com>
2020-02-03 11:44:03 -0800

    bg art

--- CellStrong/Assets/Scripts/Utils/LinearMovement.cs
+++ CellStrong/Assets/Scripts/Utils/LinearMovement.cs
@@ -9,15 +9,23 @@ public class LinearMovement : MonoBehaviour
     public Transform p1, p2;
     public float time;
 
+    public bool randomSpin;
+    public float spinSpeed;
+
     private float progress = 0;
     private void Awake()
     {
+        if (randomSpin)
+            spinSpeed = Random.RandomRange(-5.0f, 5.0f);
+        root.transform.localScale = Vector3.one * Random.RandomRange(0.5f, 2.0f);
+
         progress = 0;
         DOTween.To(() => progress, x => progress = x, 1, time).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
     }
 
     private void Update()
     {
+        root. transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
         root.transform.position = Vector3.Lerp(p1.position, p2.position, progress);
     }
 
